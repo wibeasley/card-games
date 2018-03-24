152d455b5658d2635ac8456f4d6211e513120191
Will Beasley <wibeasley@hotmail.com>
2018-03-24 17:53:29 -0500

    64 bit integers
    see #1

--- games/war.R
+++ games/war.R
@@ -13,6 +13,7 @@ requireNamespace("tidyr"        )
 requireNamespace("dplyr"        ) # Avoid attaching dplyr, b/c its function names conflict with a lot of packages (esp base, stats, and plyr).
 requireNamespace("testit"       ) # For asserting conditions meet expected patterns/conditions.
 requireNamespace("checkmate"    ) # For asserting conditions meet expected patterns/conditions. # remotes::install_github("mllg/checkmate")
+requireNamespace("VeryLargeIntegers") # For 64-bit integers, coutning the large number of turns.
 requireNamespace("OuhscMunge"   ) # remotes::install_github(repo="OuhscBbmc/OuhscMunge")
 
 # ---- declare-globals ---------------------------------------------------------
@@ -43,10 +44,10 @@ determine_rank <- function( card_name ) {
 
 print_draw <- function( c1, c2, r1, r2 ) {
   message(sprintf(
-    "Turn %i\n\tPlayer 1 card: %s (rank %i); %i cards remaining\n\tPlayer 2 card: %s (rank %i); %i cards remaining\n\tWinner: %s",
-    turn_index,
-    c1, r1, as.integer(flifo::size(deck_1)/92L),
-    c2, r2, as.integer(flifo::size(deck_2)/92L),
+    "Turn %s\n\tPlayer 1 card: %7s (rank %2i); %.0f cards remaining\n\tPlayer 2 card: %7s (rank %2i); %.0f cards remaining\n\tWinner: %s",
+    as.character(turn_index),
+    c1, r1, round(flifo::size(deck_1)/92L, 2),
+    c2, r2, round(flifo::size(deck_2)/92L, 2),
     determine_winner(r1, r2)
   ))
 }
@@ -72,6 +73,7 @@ indices_player_2 <- setdiff(seq_len(deck_count), indices_player_1) %>%
   sample()
 
 
+
 # ---- tweak-data --------------------------------------------------------------
 ds_player_1 <- ds_deck %>%
   dplyr::slice(indices_player_1)
@@ -80,6 +82,9 @@ ds_player_2 <- ds_deck %>%
   dplyr::slice(indices_player_2)
 
 rm(indices_player_1, indices_player_2)
+
+plot(ds_player_1$rank, type="l")
+lines(ds_player_2$rank, col="blue")
 
 deck_1   <- fifo(max_length = deck_count)
 deck_2   <- fifo(max_length = deck_count)
@@ -93,7 +98,7 @@ as.list(ds_player_1$card) %>%
   purrr::walk(function(x) flifo::push(deck_1, x))
 
 # ---- run ---------------------------------------------------------------------
-warning("currently, all cards are lost in a tie.  There are no winners in war.")
+warning("currently, all cards are lost in a tie.  There are no winners in war.  (Actually, I've been too lazy to program ties.)")
 # determine_winner(5,3)
 
 # escrow_1 <- list()
@@ -105,9 +110,10 @@ warning("currently, all cards are lost in a tie.  There are no winners in war.")
 # tie <- function( rank_both ) {
 #
 # }
-turn_index <- 0L
+# bit64::
+turn_index <- VeryLargeIntegers::as.vli(0L)
 while( !flifo::is.empty(deck_1) && !flifo::is.empty(deck_2) ) {
-  turn_index <- turn_index + 1L
+  turn_index <- turn_index + VeryLargeIntegers::as.vli(1)
   card_1 <- flifo::pop(deck_1)
   card_2 <- flifo::pop(deck_2)
   rank_1 <- determine_rank(card_1)
@@ -129,7 +135,15 @@ while( !flifo::is.empty(deck_1) && !flifo::is.empty(deck_2) ) {
     # flifo::push(deck_1, card_1)
     # flifo::push(deck_2, card_2)
   }
-  Sys.sleep(1)
+  Sys.sleep(1/16)
+}
+
+if( flifo::is.empty(deck_1) ) {
+  message("Player 2 Wins !!!!!!")
+} else if( flifo::is.empty(deck_2) ) {
+  message("Player 1 Wins !!!!!!")
+} else {
+  message("Both players lost on the final turn.")
 }
 
 # ---- verify-values -----------------------------------------------------------
